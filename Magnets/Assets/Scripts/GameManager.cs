527c3d6cad0da318ef73698ec8b74c28f6764f8d
tchoudhury <tchoudhury224@gmail.com>
2017-01-21 16:57:28 -0500

    Adding menu scene, load level script, particle system effects.

---
+++ Magnets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager _instance;
+
+	public static GameManager Instance{
+		get{
+			if(_instance == null){
+				_instance = new GameManager ();
+			}
+
+			return _instance;
+		}
+	}
+
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);		
+	}
+
+	public static void LoadNextScene(){
+		Scene currentScene = SceneManager.GetActiveScene(); //scenes work based off array;
+		int currentSceneCount = currentScene.buildIndex;
+		SceneManager.LoadScene(currentSceneCount++);
+	}
+}
---
+++ Magnets/Assets/Scripts/MagnetParticleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetParticleController : MonoBehaviour {
+
+	public ParticleSystem magnetWaveParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+		magnetWaveParticleSystem = FindObjectOfType<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		emitMagneticWaveParticles ();
+	}
+
+	void emitMagneticWaveParticles(){
+		if (Input.GetKey (KeyCode.A) && magnetWaveParticleSystem.isStopped) {
+			magnetWaveParticleSystem.Play();
+			Debug.Log ("emit waves");
+		} else if(Input.GetKeyUp(KeyCode.A) && magnetWaveParticleSystem.isPlaying) {
+			magnetWaveParticleSystem.Stop ();
+			Debug.Log ("Dont emit waves");
+		}
+	}
+}
